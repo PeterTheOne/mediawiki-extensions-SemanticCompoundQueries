ae0fabbc6fb85c643932a4f4c22ed4e26078e700
PeterTheOne <petertheone@gmail.com>
2016-06-22 18:08:24 +0200

    fix code style

--- SCQ_CompoundQueryApi.php
+++ SCQ_CompoundQueryApi.php
@@ -10,86 +10,87 @@ use SMW\MediaWiki\Api\ApiRequestParameterFormatter;
  *
  * @author Peter Grassberger < petertheone@gmail.com >
  */
-class SCQCompoundQueryApi extends Query {
+class SCQCompoundQueryApi extends Query
+{
 
-    /**
-     * @see ApiBase::execute
-     */
-    public function execute() {
-        $parameterFormatter = new ApiRequestParameterFormatter( $this->extractRequestParams() );
-        $parameters = $parameterFormatter->getAskApiParameters();
+	/**
+	 * @see ApiBase::execute
+	 */
+	public function execute() {
+		$parameterFormatter = new ApiRequestParameterFormatter( $this->extractRequestParams() );
+		$parameters = $parameterFormatter->getAskApiParameters();
 
-        list($queryParams, $otherParams) = SCQQueryProcessor::separateParams($parameters);
-        list($queryResult, $otherParams) = SCQQueryProcessor::queryAndMergeResults($queryParams, $otherParams);
+		list( $queryParams, $otherParams ) = SCQQueryProcessor::separateParams( $parameters );
+		list( $queryResult, $otherParams ) = SCQQueryProcessor::queryAndMergeResults( $queryParams, $otherParams );
 
-        $outputFormat = 'json';
-        if ( $this->getMain()->getPrinter() instanceof \ApiFormatXml ) {
-            $outputFormat = 'xml';
-        }
+		$outputFormat = 'json';
+		if ( $this->getMain()->getPrinter() instanceof \ApiFormatXml ) {
+			$outputFormat = 'xml';
+		}
 
-        $this->addQueryResult( $queryResult, $outputFormat );
-    }
+		$this->addQueryResult( $queryResult, $outputFormat );
+	}
 
-    /**
-     * @codeCoverageIgnore
-     * @see ApiBase::getAllowedParams
-     *
-     * @return array
-     */
-    public function getAllowedParams() {
-        return array(
-            'query' => array(
-                ApiBase::PARAM_TYPE => 'string',
-                ApiBase::PARAM_REQUIRED => true,
-            ),
-        );
-    }
+	/**
+	 * @codeCoverageIgnore
+	 * @see ApiBase::getAllowedParams
+	 *
+	 * @return array
+	 */
+	public function getAllowedParams() {
+		return array(
+			'query' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+		);
+	}
 
-    /**
-     * @codeCoverageIgnore
-     * @see ApiBase::getParamDescription
-     *
-     * @return array
-     */
-    public function getParamDescription() {
-        return array(
-            'query' => 'The multiple queries string in ask-language'
-        );
-    }
+	/**
+	 * @codeCoverageIgnore
+	 * @see ApiBase::getParamDescription
+	 *
+	 * @return array
+	 */
+	public function getParamDescription() {
+		return array(
+			'query' => 'The multiple queries string in ask-language'
+		);
+	}
 
-    /**
-     * @codeCoverageIgnore
-     * @see ApiBase::getDescription
-     *
-     * @return array
-     */
-    public function getDescription() {
-        return array(
-            'API module to query SMW by providing a multiple queries in the ask language.'
-        );
-    }
+	/**
+	 * @codeCoverageIgnore
+	 * @see ApiBase::getDescription
+	 *
+	 * @return array
+	 */
+	public function getDescription() {
+		return array(
+			'API module to query SMW by providing a multiple queries in the ask language.'
+		);
+	}
 
-    /**
-     * @codeCoverageIgnore
-     * @see ApiBase::getExamples
-     *
-     * @return array
-     */
-    protected function getExamples() {
-        return array(
-            'api.php?action=compoundquery&query=' . urlencode('[[Category:Wien]]; ?Has coordinates|[[Category:Graz]]; ?Has coordinates'),
-            'api.php?action=compoundquery&query=' . urlencode('|[[Category:Wien]]; ?Has coordinates|[[Category:Graz]]; ?Has coordinates'),
-        );
-    }
+	/**
+	 * @codeCoverageIgnore
+	 * @see ApiBase::getExamples
+	 *
+	 * @return array
+	 */
+	protected function getExamples() {
+		return array(
+			'api.php?action=compoundquery&query=' . urlencode( '[[Category:Wien]]; ?Has coordinates|[[Category:Graz]]; ?Has coordinates' ),
+			'api.php?action=compoundquery&query=' . urlencode( '|[[Category:Wien]]; ?Has coordinates|[[Category:Graz]]; ?Has coordinates' ),
+		);
+	}
 
-    /**
-     * @codeCoverageIgnore
-     * @see ApiBase::getVersion
-     *
-     * @return string
-     */
-    public function getVersion() {
-        return __CLASS__ . '-' . SCQ_VERSION;
-    }
+	/**
+	 * @codeCoverageIgnore
+	 * @see ApiBase::getVersion
+	 *
+	 * @return string
+	 */
+	public function getVersion() {
+		return __CLASS__ . '-' . SCQ_VERSION;
+	}
 
 }
